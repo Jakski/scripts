87c55b6769b522cf68b785d65625cfb98a3433a1
Jakub Pieńkowski <8525083+Jakski@users.noreply.github.com>
2020-04-26 16:29:56 +0200

    Add VM provisioning script

---
+++ provision.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+################################################################################
+# Provision fresh system image for local virtual machine
+################################################################################
+
+virt-builder \
+  debian-10 \
+  -o debian-10.qcow2 \
+  --size 50G \
+  --format qcow2 \
+  --arch x86_64 \
+  --ssh-inject root:file:"$HOME"/.ssh/local_dev.pub \
+  --root-password password:debian-10 \
+  --install haveged,neovim,dnsutils,strace,tcpdump,libcap2-bin \
+  --hostname debian-10 \
+  --timezone Europe/Warsaw \
+  --update
